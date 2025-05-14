7f3f11bdc2ccbba2caa238306a2c5381f8c5b609
RickyBic <99736616+RickyBic@users.noreply.github.com>
2025-05-14 11:12:59 +0300

    Personnalisation des formulaires

--- src/Form/ArticleForm.php
+++ src/Form/ArticleForm.php
@@ -8,21 +8,44 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ArticleForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content')
-            ->add('createdAt')
+            ->add('title', TextType::class, [
+                'label' => 'Titre de l\'article',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Entrez un titre pour votre article',
+                    'required' => true
+                ]
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu de l\'article',
+                'attr' => [
+                    'class' => 'form-control',
+                    'rows' => 10,
+                    'placeholder' => 'Écrivez le contenu de votre article ici...',
+                    'required' => true
+                ]
+            ])
             ->add('categories', EntityType::class, [
                 'class' => Category::class,
-                'choice_label' => 'id',
+                'choice_label' => 'title',
                 'multiple' => true,
-            ])
-        ;
+                'expanded' => true,
+                'label' => 'Catégories',
+                'attr' => [
+                    'class' => 'form-check',
+                ],
+                'label_attr' => [
+                    'class' => 'form-check-label'
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
--- src/Form/CommentForm.php
+++ src/Form/CommentForm.php
@@ -2,26 +2,38 @@
 
 namespace App\Form;
 
-use App\Entity\Article;
 use App\Entity\Comment;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CommentForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('author')
-            ->add('content')
-            ->add('createdAt')
-            ->add('article', EntityType::class, [
-                'class' => Article::class,
-                'choice_label' => 'id',
+            ->add('author', TextType::class, [
+                'label' => 'Votre nom',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Votre nom',
+                    'required' => true
+                ]
             ])
-        ;
+            ->add('content', TextareaType::class, [
+                'label' => 'Votre commentaire',
+                'attr' => [
+                    'class' => 'form-control',
+                    'rows' => 4,
+                    'placeholder' => 'Écrivez votre commentaire ici...',
+                    'required' => true
+                ]
+            ]);
+
+        // Note: Le champ 'article' n'est pas ajouté ici car il sera défini
+        // automatiquement dans le contrôleur avec $comment->setArticle($article)
     }
 
     public function configureOptions(OptionsResolver $resolver): void
